c87df32aa28ebd6fc87990a61ecc830bbb2f425b
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-03-06 14:10:07 +0100

    Added indexer for the creator's fullname.

--- ftw/solr/indexers.py
+++ ftw/solr/indexers.py
@@ -1,6 +1,7 @@
 from zope.component import getMultiAdapter
 from zope.interface import Interface
 from plone.indexer import indexer
+from Products.CMFCore.utils import getToolByName
 
 
 @indexer(Interface)
@@ -15,3 +16,15 @@ def site_section(obj, **kwargs):
         section = navroot.unrestrictedTraverse(contentPath[0])
         return section.Title()
     return ''
+
+
+@indexer(Interface)
+def creator_fullname(obj, **kwargs):
+    """An indexer which returns the full name of the content creator.
+    """
+    creator = obj.Creator()
+    mtool = getToolByName(obj, 'portal_membership')
+    member_info = mtool.getMemberInfo(creator)
+    if member_info and member_info['fullname']:
+        return member_info['fullname']
+    return creator
