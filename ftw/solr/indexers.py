5aa767e1295a29d6b9a2006ac5ab36516a048e70
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-03-06 18:54:52 +0100

    Added indexer for SearchableText which does not include 'id', 'title', 'description' and 'searchwords'.

--- ftw/solr/indexers.py
+++ ftw/solr/indexers.py
@@ -2,6 +2,7 @@ from zope.component import getMultiAdapter
 from zope.interface import Interface
 from plone.indexer import indexer
 from Products.CMFCore.utils import getToolByName
+from ZODB.POSException import ConflictError
 
 
 @indexer(Interface)
@@ -28,3 +29,50 @@ def creator_fullname(obj, **kwargs):
     if member_info and member_info['fullname']:
         return member_info['fullname']
     return creator
+
+@indexer(Interface)
+def searchable_text(obj, **kwargs):
+    """A SearchableText indexer that does not include id, title and 
+       description.
+    """
+    data = []
+    charset = obj.getCharset()
+    for field in obj.Schema().fields():
+        if not field.searchable:
+            continue
+        if field.getName() in ['id', 'title', 'description', 'searchwords']:
+            continue
+        method = field.getIndexAccessor(obj)
+        try:
+            datum =  method(mimetype="text/plain")
+        except TypeError:
+            # Retry in case typeerror was raised because accessor doesn't
+            # handle the mimetype argument
+            try:
+                datum =  method()
+            except (ConflictError, KeyboardInterrupt):
+                raise
+            except:
+                continue
+        if datum:
+            vocab = field.Vocabulary(obj)
+            if isinstance(datum, (list, tuple)):
+                # Unmangle vocabulary: we index key AND value
+                vocab_values = map(lambda value, vocab=vocab: vocab.getValue(value, ''), datum)
+                datum = list(datum)
+                datum.extend(vocab_values)
+                datum = ' '.join(datum)
+            elif isinstance(datum, basestring):
+                if isinstance(datum, unicode):
+                    datum = datum.encode(charset)
+                value = vocab.getValue(datum, '')
+                if isinstance(value, unicode):
+                    value = value.encode(charset)
+                datum = "%s %s" % (datum, value, )
+
+            if isinstance(datum, unicode):
+                datum = datum.encode(charset)
+            data.append(str(datum))
+
+    data = ' '.join(data)
+    return data
