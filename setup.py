76d75af2a0144ab3462c201a7c071f5c417dd821
Jonas Baumann <jone@jone.ch>
2012-11-12 14:58:25 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,28 +6,33 @@ version = '1.0a2.dev0'
 setup(name='ftw.solr',
       version=version,
       description="Solr integration for Plone using collective.solr",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
+
+      keywords='ftw solr',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.solr',
-      license='GPL',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'collective.solr',
-          'collective.monkeypatcher',
-          'plone.app.contentlisting',
-          'plone.app.search',
-      ],
+        'setuptools',
+        'collective.solr',
+        'collective.monkeypatcher',
+        'plone.app.contentlisting',
+        'plone.app.search',
+        ],
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
