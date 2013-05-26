1a6ea1a4089653d70a9147e600a5d99830b0eef3
Thomas Buchberger <t.buchberger@4teamwork.ch>
2013-05-26 20:30:31 +0200

    Added tests for snippetText indexer.

---
+++ ftw/solr/tests/test_indexers.py
@@ -0,0 +1,47 @@
+from ftw.solr.testing import SOLR_FUNCTIONAL_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from unittest2 import TestCase
+from plone.indexer.wrapper import IndexableObjectWrapper
+
+
+class TestSnippetText(TestCase):
+
+    layer = SOLR_FUNCTIONAL_TESTING
+
+    def test_id_not_in_snippet_text(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        doc = portal[portal.invokeFactory('Document', 'doc1',
+                                          title=u"Document 1")]
+        wrapped = IndexableObjectWrapper(doc, portal.portal_catalog)
+        self.assertNotIn('doc1', wrapped.snippetText)
+
+    def test_title_not_in_snippet_text(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        doc = portal[portal.invokeFactory('Document', 'doc1',
+                                          title=u"Document 1")]
+        wrapped = IndexableObjectWrapper(doc, portal.portal_catalog)
+        self.assertNotIn('Document 1', wrapped.snippetText)
+
+    def test_body_text_in_snippet_text(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        doc = portal[portal.invokeFactory('Document', 'doc1',
+                                          title=u"Document 1",
+                                          text=u"Body Text")]
+        wrapped = IndexableObjectWrapper(doc, portal.portal_catalog)
+        self.assertIn('Body Text', wrapped.snippetText)
+
+    def test_strip_html_tags(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        doc = portal[portal.invokeFactory('Document', 'doc1')]
+        def SearchableText(self):
+            return "<p>Body Text</p>"
+        doc.SearchableText = SearchableText.__get__(doc, doc.__class__) 
+        wrapped = IndexableObjectWrapper(doc, portal.portal_catalog)
+        self.assertIn('Body Text', wrapped.snippetText)
+        self.assertNotIn('<p>', wrapped.snippetText)
+        self.assertNotIn('</p>', wrapped.snippetText)
