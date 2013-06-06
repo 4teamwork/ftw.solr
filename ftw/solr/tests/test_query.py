2ecb22c19ed306dce3114ccf789df860effe63eb
Thomas Buchberger <t.buchberger@4teamwork.ch>
2013-06-06 09:21:37 +0200

    Added tests for buildQuery patch which espaces slashes in paths.

---
+++ ftw/solr/tests/test_query.py
@@ -0,0 +1,41 @@
+from collective.solr.interfaces import ISolrConnectionConfig
+from collective.solr.manager import SolrConnectionConfig
+from collective.solr.manager import SolrConnectionManager
+from collective.solr.search import Search
+from collective.solr.tests.utils import getData, fakehttp
+from ftw.solr.patches.search import buildQuery
+from unittest import TestCase
+from zope.component import provideUtility
+
+
+class TestBuildQuery(TestCase):
+
+    def setUp(self):
+        provideUtility(SolrConnectionConfig(), ISolrConnectionConfig)
+        self.mngr = SolrConnectionManager()
+        self.mngr.setHost(active=True)
+        conn = self.mngr.getConnection()
+        fakehttp(conn, getData('plone_schema.xml')) # fake schema response
+        self.mngr.getSchema()                       # read and cache the schema
+        self.search = Search()
+        self.search.manager = self.mngr
+
+        # Patch buildQuery method
+        self.search.buildQuery = buildQuery.__get__(self.search,
+                                                    self.search.__class__)
+
+    def tearDown(self):
+        self.mngr.closeConnection()
+        self.mngr.setHost(active=False)
+
+    def build_query(self, *args, **kwargs):
+        query = self.search.buildQuery(*args, **kwargs)
+        return ' '.join(sorted(query.values()))
+
+    def test_simple_path_query(self):
+        self.assertEquals('+path_parents:\\/spam\\/and\\/eggs',
+                          self.build_query(path_parents='/spam/and/eggs'))
+
+    def test_path_list_query(self):
+        self.assertEquals('+path_parents:(\\/spam OR \\/eggs)',
+                          self.build_query(path_parents=['/spam', '/eggs']))
