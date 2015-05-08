9d9a9a8995f3d096648e090de68c8da4189563e3
Jonas Baumann <jone@jone.ch>
2015-05-08 12:10:04 +0200

    4teamwork is an AG.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='ftw.solr',
         ],
 
       keywords='ftw solr',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.solr',
       license='GPL2',
