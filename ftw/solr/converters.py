429a5c96d7cb54f8c791018d53423888a299052f
Thomas Buchberger <t.buchberger@4teamwork.ch>
2018-01-06 15:57:51 +0100

    Improve converters and add tests

--- ftw/solr/converters.py
+++ ftw/solr/converters.py
@@ -1,26 +1,28 @@
 from DateTime import DateTime
 from datetime import datetime
+from datetime import date
 
 
-def datetime_converter(value, multivalued):
+def to_iso8601(value, multivalued=False):
     if isinstance(value, DateTime):
         v = value.toZone('UTC')
-        value = '%04d-%02d-%02dT%02d:%02d:%06.3fZ' % (
+        value = u'%04d-%02d-%02dT%02d:%02d:%06.3fZ' % (
             v.year(), v.month(), v.day(), v.hour(), v.minute(), v.second()
         )
     elif isinstance(value, datetime):
-        # Convert a timezone aware timetuple to a non timezone aware time
-        # tuple representing utc time. Does nothing if object is not
-        # timezone aware
-        value = datetime(*value.utctimetuple()[:7])
-        value = '%s.%03dZ' % (
+        if value.tzinfo is not None:
+            # Convert to timezone naive in UTC
+            value = (value - value.utcoffset()).replace(tzinfo=None)
+        value = u'%s.%03dZ' % (
             value.strftime('%Y-%m-%dT%H:%M:%S'),
-            value.microsecond % 1000
+            value.microsecond / 1000
         )
+    elif isinstance(value, date):
+        value = value.strftime('%Y-%m-%dT%H:%M:%S.000Z').decode()
     return value
 
 
-def str_converter(value, multivalued):
+def to_unicode(value, multivalued=False):
     if isinstance(value, tuple):
         value = list(value)
     if not isinstance(value, list):
@@ -31,13 +33,13 @@ def str_converter(value, multivalued):
         value[i] = v
     if not multivalued:
         if len(value) > 1:
-            return u', '.join(value)
+            return u' '.join(value)
         else:
             return value[0]
     return value
 
 
 CONVERTERS = {
-    'solr.DatePointField': datetime_converter,
-    'solr.StrField': str_converter,
+    'solr.DatePointField': to_iso8601,
+    'solr.StrField': to_unicode,
 }
