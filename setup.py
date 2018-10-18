b4c3f63cbcc580dfd1f9f2bc8c3a4bf2c48c1718
Thomas Buchberger <t.buchberger@4teamwork.ch>
2018-10-18 17:06:56 +0200

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='ftw.solr',
-    version='2.3.0',
+    version='2.3.1.dev0',
     description="Solr integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
