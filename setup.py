e8b623b6e13fe831227a6ec11493af0960aedfbe
Thomas Buchberger <t.buchberger@4teamwork.ch>
2019-10-25 14:06:04 +0200

    Add changelog entry and pypi classifiers for Plone 5.0/5.1

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",
