ceeb0e04ef512ee7386548d85e632781d35fcd3c
David Erni <d.erni@4teamwork.ch>
2020-03-26 18:33:27 +0100

    Fix rounding errors with DateTime milliseconds.
    When milliseconds of a `DateTime` are above 999500 python string
    formating rounds up the value. This leads to an ISO 8601 date format
    strings with a value of 60 for the [ss] (second ) part. Solr considers
    such date format strings invalid and throws an error, e.g.:
    
      Invalid Date in Date Math String:'2015-04-07T14:43:60.000Z'
    
    This will result in the corresponding object not being indexed or
    updated in solr.
    
    We fix the issue by splitting the `seconds` part of a `DateTime` into
    separate seconds and milliseconds parts and also pass them as separate
    arguments to string formatting.

--- ftw/solr/converters.py
+++ ftw/solr/converters.py
@@ -1,13 +1,16 @@
 from DateTime import DateTime
 from datetime import datetime
 from datetime import date
+import math
 
 
 def to_iso8601(value, multivalued=False):
     if isinstance(value, DateTime):
         v = value.toZone('UTC')
-        value = u'%04d-%02d-%02dT%02d:%02d:%06.3fZ' % (
-            v.year(), v.month(), v.day(), v.hour(), v.minute(), v.second()
+        millisecond, second = math.modf(v.second())
+        value = u'%04d-%02d-%02dT%02d:%02d:%02d.%03dZ' % (
+            v.year(), v.month(), v.day(), v.hour(), v.minute(),
+            int(second), millisecond * 1000
         )
     elif isinstance(value, datetime):
         if value.tzinfo is not None:
