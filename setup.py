f4455b2cb79081e5f9ac90252264c50f4a1846b1
Lukas Graf <lukas.graf@4teamwork.ch>
2023-04-06 10:21:45 +0200

    Update PyPi classifiers.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
